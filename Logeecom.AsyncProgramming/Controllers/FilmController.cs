d6c90540ce75690e0597321eec06db25e041b62f
Luka Simovic <luka.simovic@logeecom.com>
2022-06-27 11:37:12 +0200

    Rename controller

--- Logeecom.AsyncProgramming/Controllers/FilmController.cs
+++ Logeecom.AsyncProgramming/Controllers/FilmController.cs
@@ -1,10 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Logeecom.AsyncProgramming.Business.Models;
+using Logeecom.AsyncProgramming.Business.Services;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Logeecom.AsyncProgramming.Controllers
+namespace Logeecom.AsyncProgramming.Presentation.Controllers
 {
-    [Route("api/asdasd/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class FilmController : ControllerBase
     {
+        private readonly FilmServiceSync filmService;
+
+        public FilmController(FilmServiceSync filmService)
+        {
+            this.filmService = filmService;
+        }
+
+        [HttpPost]
+        public string AddFilms(List<FilmViewModel> films)
+        {
+            System.Diagnostics.Stopwatch? watch = new();
+
+            watch.Start();
+
+            this.filmService.CreateFilms(films);
+
+            watch.Stop();
+
+            return $"Execution Time: {watch.ElapsedMilliseconds} ms";
+        }
+
+        [HttpDelete]
+        public void DeleteAll()
+        {
+            this.filmService.DeleteAll();
+        }
     }
 }
